fb15128bda0789c874fad4abaf9c5984530fe570
shisirthakuri <shahishisir087@gmail.com>
2025-02-28 23:02:58 +0545

    first commit

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ const express = require("express") //requiring express package
 const app = express() // storing it in app app vanne variable throught use gareko
 
 
-app.set("view engine", "ejs")
+
 
 //  / ma gayo vane k dekhaune and request garepachhi response garnai parxa hai keta ho
 
 app.get('/',(req,res)=>{
-    res.send("home")
+    res.send("hi there people")
 })
 
-app.get('/about',(req,res)=>{
-    res.render("this is the about section ")
-
-
-})
 
 app.get('/contact', function(req,res){
-    res.send("this is the contact page")
+    res.status(200).json({
+        massege:"this massege from contact page hai"
+    })
 })
 
 
